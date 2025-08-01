5de52bcfb34578f87ff9c02bfb2ac00baa2cfd38
Yuri Kuznetsov <playeer2905@gmail.com>
2025-08-01 21:42:35 +0300

    add prettier

--- codegen.ts
+++ codegen.ts
@@ -1,11 +1,11 @@
-import type { CodegenConfig } from "@graphql-codegen/cli"
+import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-    schema: "http://localhost:1337/graphql", // Strapi GraphQL endpoint
-    documents: "graphql/**/*.graphql", // Папка с твоими .graphql запросами
+    schema: 'http://localhost:1337/graphql', // Strapi GraphQL endpoint
+    documents: 'graphql/**/*.graphql', // Папка с твоими .graphql запросами
     generates: {
-        "./lib/graphql/generated.ts": {
-            plugins: ["typescript", "typescript-operations", "typescript-react-apollo"],
+        './lib/graphql/generated.ts': {
+            plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
             config: {
                 withHooks: true, // Генерировать useQuery, useMutation
                 withHOC: false,
--- shared/components/article.tsx
+++ shared/components/article.tsx
@@ -1,6 +1,6 @@
-import { useGetArticlesQuery } from "@/lib/graphql/generated"
-import { gql, useQuery } from "@apollo/client"
-import React from "react"
+import { useGetArticlesQuery } from '@/lib/graphql/generated';
+import { gql, useQuery } from '@apollo/client';
+import React from 'react';
 
 const GET_ARTICLES = gql`
     query GetArticles {
@@ -13,13 +13,13 @@ const GET_ARTICLES = gql`
             slug
         }
     }
-`
+`;
 export const Article = () => {
-    const { loading, error, data } = useGetArticlesQuery()
+    const { loading, error, data } = useGetArticlesQuery();
 
-    if (loading) return <p>Загрузка...</p>
-    if (error) return <p>Ошибка: {error.message}</p>
+    if (loading) return <p>Загрузка...</p>;
+    if (error) return <p>Ошибка: {error.message}</p>;
 
-    console.log(data)
-    return <h1>{data?.articles.length ? data.articles[0].title : null}</h1>
-}
+    console.log(data);
+    return <h1>{data?.articles.length ? data.articles[0].title : null}</h1>;
+};
